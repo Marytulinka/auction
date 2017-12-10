cee682432c21c5dd0868273d4f7bf1bdd71c5d22
maria_tulina <mar_tul@gmail.com>
2017-12-11 01:09:47 +0400

    init commit

--- src/org/egar/auction/view/Menu.java
+++ src/org/egar/auction/view/Menu.java
@@ -4,13 +4,27 @@ public class Menu {
 
     public static void showMainMenu() {
 
-        System.out.print("Выберите роль для работы: \n Гость - 1;\n Пользователь - 2;\n Администратор - 3.\n");
+        System.out.print("Выберите действие:\nПросмотр каталога лотов по категориям - 1;\n" +
+                "Войти как администратор для просмотра статистики - 2;\nВойти для участия в аукционе - 3.\n");
+    }
+
+    /*public static void showItemMenu() {
+
+        System.out.println("Для возврата в меню введите - 1;\n");
+        System.out.println();
+    }*/
+
+    public static void searchSortingAndShowBids() {
+
+        System.out.println("Для сортировки по наименованию введите - 1;\nДля поиска лота в каталоге - 2;\nПросмотр списка ставок по лоту - 3;\nПропустить этот шаг - 3.");
+        System.out.println();
     }
 
 
     public static void showUserMenu() {
 
-        System.out.println("Выберите действие: \n Просмотр каталога товаров - 1;\n Участие в аукционе - 2;\n Просмотреть 'Мои покупки' - 3");
+        System.out.println("Выберите действие: \n Участие в аукционе - 1;\n Просмотр лотов и ставок пользователя - 2;\n Просмотра корзины пользователя - 3;\n " +
+                "Расчеты - 4.\n");
         System.out.println();
     }
 
