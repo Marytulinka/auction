eb09de0dad799f0b0257264a7ab21541196eec1e
maria_tulina <mar_tul@gmail.com>
2017-12-11 01:08:33 +0400

    init commit

--- src/org/egar/auction/view/ShowItems.java
+++ src/org/egar/auction/view/ShowItems.java
@@ -5,37 +5,60 @@ import org.egar.auction.model.Category;
 import org.egar.auction.model.Item;
 import org.egar.auction.storage.Storage;
 
+
 public class ShowItems {
 
     public static void viewItems() { //каталог товаров по категориям
 
-        System.out.println("Каталог товаров: \n");
-        for (Category i : Category.values()) {
-            System.out.println(i.getDescription() + ":");
-            System.out.println();
-            for (Item item : Storage.items) {
-                if (i.equals(item.getCategory())) {
-                    System.out.println(item.getName() + "; код:" + item.getCode() + "; цена: "
-                            + item.getPrice());
+        if (Storage.items.isEmpty()) System.out.println("Аукцион закрыт. Нет данных просмотра.");
+        else {
+            System.out.println("Каталог товаров: \n");
+            for (Category i : Category.values()) {
+                System.out.println(i.getDescription() + ":");
+                System.out.println();
+                for (Item item : Storage.items) {
+                    if (!item.isSolded()) { //показываем только непроданный товар
+                        if (i.equals(item.getCategory())) {
+                            System.out.println(item.getName() + "; код:" + item.getCode() + "; цена: "
+                                    + item.getPrice());
+                        }
+                    }
                 }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 
-    public static void viewBidOfItem(int code){
+    public static void searhFromItemList(String name) {
+
+        if (Storage.items.isEmpty()) System.out.println("Аукцион закрыт. Нет данных просмотра.");
+        for (Item item : Storage.items) {
+            if (name.equalsIgnoreCase(item.getName())) {
+                if (!item.isSolded())
+                    System.out.println("Наименование товара: " + item.getName()
+                            + "; категория товара: " + item.getCategory().getDescription() + "; код:" + item.getCode() + "; цена: " + item.getPrice() + "; есть в наличии: да");
+                else System.out.println("Наименование товара: " + item.getName()
+                        + "; категория товара: " + item.getCategory() + "; цена: " + item.getPrice() + "; есть в наличии: нет");
+            }
+        }
+    }
+
+    public static void viewBidOfItem(int code) {
 
         Item itemForAuction = null;
         for (Item item : Storage.items) {
             if (code == (item.getCode())) itemForAuction = item;
         }
 
-        if(Storage.bids.isEmpty()) System.out.println("Пока нет ставок");
+        if (itemForAuction.isSolded()) System.out.println("Лот:" + itemForAuction.getName() + " (код:" + itemForAuction.getCode() + ") - продан");
         else {
-            for (Bid bid : Storage.bids) {
-                if (code == bid.getCode()) {
-                    System.out.println("лот: " + itemForAuction.getName() + " ставки:\n");
-                    System.out.println("Пользователь: " + bid.getUser().getUserName() + ", ставка: " + bid.getValue());
+            if (Storage.bids.isEmpty()) System.out.println("Лот:" + itemForAuction.getName() + " (код:" + itemForAuction.getCode() + ") - пока нет ставок");
+            else {
+                for (Bid bid : Storage.bids) {
+                    if (code == bid.getCode()) {
+                        System.out.println("Лот: " + itemForAuction.getName() + " (код:" + itemForAuction.getCode() + ") ставки:\n");
+                        System.out.println("Пользователь: " + bid.getUser().getUserName() + ", ставка: " + bid.getValue());
+                    }
                 }
             }
         }
