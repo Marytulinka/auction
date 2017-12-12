916667198dbc89035fd6e729337d2a2425248d65
Mary Tulina <petitemariet@gmail.com>
2017-12-12 13:39:32 +0400

    init commit

--- src/org/egar/auction/controller/UserService.java
+++ src/org/egar/auction/controller/UserService.java
@@ -1,6 +1,7 @@
 package org.egar.auction.controller;
 
 import org.egar.auction.model.Bid;
+import org.egar.auction.model.Purchase;
 import org.egar.auction.model.Role;
 import org.egar.auction.model.User;
 import org.egar.auction.storage.Storage;
@@ -25,20 +26,23 @@ public class UserService {
 
     public static void statistics() {
 
-        int amountOfItems = 0;
-        int amountOfBids = 0;
         System.out.println("Статистика по пользователям: \n");
         if (Storage.bids.isEmpty()) System.out.println("Нет данных для просмотра");
+        else System.out.println("-- Пользователь -- Кол-во его лотов -- Кол-во его ставок");
         for (User user : Storage.users) {
-            if(user.getRole() == Role.USER) {
-                for (Bid bid : Storage.bids) {
-                    if (user.getLogin().equals(bid.getUser().getLogin())) {
-                        amountOfItems +=bid. ;
-                        amountOfBids += ;
-                    }
+            int amountOfItems = 0;
+            int amountOfBids = 0;
+            if (user.getRole() == Role.USER) {
+                for (Purchase purchase : Storage.purchase) {
+                    if (user.getLogin().equals(purchase.getUser().getLogin()))
+                        amountOfItems += 1;
                 }
-                System.out.println(user.getUserName() + " " + amountOfItems + " " + amountOfBids);
+                for (Bid bid : Storage.bids) {
+                    if (user.getLogin().equals(bid.getUser().getLogin()))
+                        amountOfBids += 1;
+                }
             }
+            System.out.println(user.getUserName() + " " + amountOfItems + " " + amountOfBids);
         }
     }
 }
