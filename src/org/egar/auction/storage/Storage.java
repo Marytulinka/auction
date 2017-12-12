7a07bb45f344622b36e929edeebb6eb502370474
Mary Tulina <petitemariet@gmail.com>
2017-12-12 13:39:14 +0400

    init commit

--- src/org/egar/auction/storage/Storage.java
+++ src/org/egar/auction/storage/Storage.java
@@ -20,8 +20,8 @@ public class Storage {
     public static List<Item> items = new ArrayList<>();
 
     static {
-        items.add(new Item("Ботинки для горных лыж", Category.EQUIPMENT, 20810, 1, false));
-        items.add(new Item("Горные лыжи", Category.EQUIPMENT, 37806, 2, false));
+        items.add(new Item("Ботинки для горных лыж", Category.EQUIPMENT, 20810, 1, true));
+        items.add(new Item("Горные лыжи", Category.EQUIPMENT, 37806, 2, true));
         items.add(new Item("Палки для горных лыж", Category.EQUIPMENT, 2710, 3, false));
         items.add(new Item("Варежки", Category.ACCESSORIES, 1750, 4, false));
         items.add(new Item("Балаквала", Category.ACCESSORIES, 2300, 5, false));
@@ -34,11 +34,20 @@ public class Storage {
     public static List<Bid> bids = new ArrayList<>();
 
     static {
-        bids.add(new Bid(7800,users.get(0) , 7));
-        bids.add(new Bid(2000,users.get(0) , 8));
-        bids.add(new Bid(1700,users.get(1) , 9));
+        bids.add(new Bid(29000,users.get(0) , 1));
+        bids.add(new Bid(28900,users.get(1) , 1));
+        bids.add(new Bid(37900,users.get(0) , 2));
+        bids.add(new Bid(38000,users.get(1) , 2));
+
 
     }
 
-    public static List<Item> purchase = new ArrayList<>();
+    public static List<Purchase> purchase = new ArrayList<>();
+
+    static{
+        purchase.add(new Purchase(items.get(0),users.get(0)));
+        purchase.add(new Purchase(items.get(1),users.get(1)));
+
+
+    }
 }
