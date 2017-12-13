25488eb3ecef2cd103f71e190ae7a0751f704161
Mary Tulina <petitemariet@gmail.com>
2017-12-13 12:36:49 +0400

    init commit

--- src/org/egar/auction/controller/UserService.java
+++ src/org/egar/auction/controller/UserService.java
@@ -23,29 +23,6 @@ public class UserService {
         }
         System.out.print("");
     }
-
-    public static void statistics() {
-
-        System.out.println("Статистика по пользователям: \n");
-        if (Storage.bids.isEmpty()&&Storage.purchase.isEmpty()) {
-            System.out.println("Нет данных для просмотра");
-        } else System.out.println("-- Пользователь -- Кол-во его лотов -- Кол-во его ставок");
-        for (User user : Storage.users) {
-            int amountOfItems = 0;
-            int amountOfBids = 0;
-            if (user.getRole() == Role.USER) {
-                for (Purchase purchase : Storage.purchase) {
-                    if (user.getLogin().equals(purchase.getUser().getLogin()))
-                        amountOfItems += 1;
-                }
-                for (Bid bid : Storage.bids) {
-                    if (user.getLogin().equals(bid.getUser().getLogin()))
-                        amountOfBids += 1;
-                }
-            }
-             System.out.println(user.getUserName() + " " + amountOfItems + " " + amountOfBids);
-        }
-    }
 }
 
 
