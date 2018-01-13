0ae285986e2b95e27de00104ce289de308dbfbb6
maria_tulina <mar_tul@gmail.com>
2018-01-13 21:41:51 +0400

    init commit

--- src/org/egar/auction/model/Purchase.java
+++ src/org/egar/auction/model/Purchase.java
@@ -1,17 +1,30 @@
 package org.egar.auction.model;
 
+/**
+ * Характеристики покупок: купленный товар; пользователь,купивший товар; признак доставки товара покупателю
+ */
 public class Purchase {
 
     private Item item;
-    private User user; //покупатель
+    private User user;
+    private boolean delivered;
 
-    public Purchase(Item item, User user){
-        this.item=item;
-        this.user=user;
+    public Purchase(Item item, User user, boolean delivered){ //заносим в коллекцию инфо о покупке
+        this.item = item;
+        this.user = user;
+        this.delivered = delivered;
 
+    }
+
+    public boolean isDelivered(){
+        return delivered;
     }
 
     public User getUser(){
         return user;
+    }
+
+    public Item getItem () {
+        return item;
     }
 }
