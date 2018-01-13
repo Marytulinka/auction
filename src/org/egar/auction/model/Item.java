c5a8202006db1572d938a6b3b27b5544b7140f91
maria_tulina <mar_tul@gmail.com>
2018-01-13 21:41:25 +0400

    init commit

--- src/org/egar/auction/model/Item.java
+++ src/org/egar/auction/model/Item.java
@@ -1,7 +1,7 @@
 package org.egar.auction.model;
 
 /**
- * Характеристики товара: наименование, цена, категория, код
+ * Характеристики товара: наименование, цена, категория, код, признак продаже товара
  */
 public class Item {
 
@@ -10,13 +10,15 @@ public class Item {
     private float price;
     private int code;
     private boolean solded;
+    private float priceForDelivery;
 
-    public Item(String name, Category category, float price, int code, boolean solded) {
+    public Item(String name, Category category, float price, int code, boolean solded, float priceForDelivery) {
         this.name = name;
         this.category = category;
         this.price = price;
         this.code = code;
         this.solded = solded;
+        this.priceForDelivery = priceForDelivery;
     }
 
     public String getName() {
@@ -37,6 +39,10 @@ public class Item {
 
     public boolean isSolded() {
         return solded;
+    }
+
+    public float getPriceForDelivery() {
+        return priceForDelivery;
     }
 }
 
